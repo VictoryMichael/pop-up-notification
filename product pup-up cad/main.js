88259624fbfe4c588d3e995050bebf7ab243684d
Victory  Michael <98047104+VictoryMichael@users.noreply.github.com>
2022-08-26 03:37:05 -0700

    Add files via upload

---
+++ product pup-up cad/main.js
@@ -0,0 +1,19 @@
+const popUp = document.getElementById('popup');
+
+function openTab() {
+popUp.classList.add('open-pupup')
+
+}
+
+function closeTab(){
+    popUp.classList.remove('open-pupup')
+}
+
+
+
+
+
+
+
+
+
